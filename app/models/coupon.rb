c53ffe741ab1f355d13c3df91308a873abd1b44c
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-03-09 17:48:57 +0000

    first seed ok, relationship in models and gem rails-cors

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,4 @@
 class Coupon < ApplicationRecord
   belongs_to :restaurant
+  has_and_belongs_to_many :tags
 end
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,14 @@ module RailsBackEnd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    # configuration for rails-cors.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
