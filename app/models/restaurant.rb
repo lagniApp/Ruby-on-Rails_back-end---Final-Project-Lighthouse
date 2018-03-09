8cd3ab2f980941fe6973fe5afe212e812034e197
tjbeirao <tjbeirao@gmail.com>
2018-03-09 21:22:23 +0000

    added .env file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,9 +3,9 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :username, presence: true
-  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :email, presence: true
   validates :password, presence: true
-  # validates :phone, format: { with: \A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z }
+  validates :phone, presence: true
   validates :address, presence: true
 
 end
