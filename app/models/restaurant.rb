b0e9409ce33d9278c32d3b36e7798872215f18fc
tjbeirao <tjbeirao@gmail.com>
2018-03-09 20:30:58 +0000

    api to convert address to longitude and latitude implemented

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
   has_many :coupons
+
+  validates :name, presence: true
+  validates :username, presence: true
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :password, presence: true
+  validates :phone, format: { with: \A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z }
+  validates :address, presence: true
+
 end
